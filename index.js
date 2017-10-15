556ea51b858034a56b1bf01e5c7f631a556fcce1
Brandon McFarland <branmcf@Brandons-MBP.attlocal.net>
2017-10-15 17:50:54 -0500

    Added db url to env, created api dir in root, modified package.json by installing morgan and body parser. Moved code out of index.js in root to index.js in /api.

--- index.js
+++ index.js
@@ -1,18 +1,85 @@
-var express = require('express');
-var app = express();
+'use strict';
 
-app.set('port', (process.env.PORT || 5000));
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const api = require('./api/index');
+const app = express();
 
-app.use(express.static(__dirname + '/public'));
+/*******************************************************************************
+MIDDLEWARE
+*******************************************************************************/
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// Use Morgan request logger
+app.use(logger(app.get("env") === "production" ? "combined" : "dev"));
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+// For parsing application/json
+app.use(bodyParser.json());
+
+// For parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Allow client applications from other domains use API Server (CORS)
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+/*******************************************************************************
+API
+*******************************************************************************/
+
+app.use('/', api);
+
+/*******************************************************************************
+SETUP
+*******************************************************************************/
+
+// Set env, host, and port
+app.set("env", process.env.NODE_ENV || "development");
+app.set("host", process.env.HOST || "0.0.0.0");
+app.set("port", process.env.PORT || 5000);
+
+// Listen on port
+app.listen(app.get("port"), function () {
+    console.log('\n' + '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+    console.log('~ PoVal API running on port ' + app.get("port") + ' ~');
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~' + '\n');
 });
+
+/*******************************************************************************
+ERROR HANDLING
+*******************************************************************************/
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// development error handler, will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status( err.code || 500 )
+        .json({
+            status: 'error',
+            message: err
+        });
+    });
+}
+
+// production error handler, will NOT print stacktrace
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500)
+    .json({
+        status: 'error',
+        message: err.message
+    });
+});
+
+/*******************************************************************************
+EXPORTS
+*******************************************************************************/
+module.exports = app;
